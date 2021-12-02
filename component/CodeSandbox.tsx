f25b221b8fe7a41df4eef87a15ba4a3831916410
holtzy <yan.holtz@datadoghq.com>
2021-12-02 18:04:07 +0100

    sandbox works

--- component/CodeSandbox.tsx
+++ component/CodeSandbox.tsx
@@ -8,7 +8,7 @@ type CodeSandboxProps = {
 
 export const CodeSandbox = ({ vizName }: CodeSandboxProps) => {
   const url =
-    "https://codesandbox.io/embed/github/holtzy/react-graph-gallery/tree/main/pages/viz/" +
+    "https://codesandbox.io/embed/github/holtzy/react-graph-gallery/tree/main/viz/" +
     vizName +
     "?fontsize=14&hidenavigation=1&theme=dark&expanddevtools=0";
 
@@ -28,10 +28,3 @@ export const CodeSandbox = ({ vizName }: CodeSandboxProps) => {
     ></iframe>
   );
 };
-<iframe
-  src="https://codesandbox.io/embed/github/holtzy/react-graph-gallery/tree/main/pages/viz/CircularPackingBasic?expanddevtools=1&fontsize=14&theme=dark"
-  style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
-  title="violin-plot-basic"
-  allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-  sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-></iframe>;
