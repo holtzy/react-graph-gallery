a6bf53482564a3c489274f829c4a0b4c34ef166a
holtzy <yan.holtz@datadoghq.com>
2021-12-10 14:53:51 +0100

    add missing data

--- viz/BoxplotToViolinTransition/index.js
+++ viz/BoxplotToViolinTransition/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import { BoxplotToViolinTransition } from "./BoxplotToViolinTransition";
+import { data } from "./data/distribution-multi-groups-random";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<BoxplotToViolinTransition width={800} height={300} />, rootElement);
+ReactDOM.render(<BoxplotToViolinTransition width={800} height={300} data={data}/>, rootElement);
