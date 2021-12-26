0a4692f17c3f681105c8036d9857b4544c3b8038
holtzy <yan.holtz@datadoghq.com>
2021-12-26 21:44:48 +0100

    more axis blogpost

--- pages/build-axis-with-react.tsx
+++ pages/build-axis-with-react.tsx
@@ -5,51 +5,112 @@ import Contact from "../component/Contact";
 import { ChartOrSandbox } from "../component/ChartOrSandbox";
 import ChartFamilySection from "../component/ChartFamilySection";
 import { AccordionSection } from "../component/AccordionSection";
-import { data as densityChartData } from "../data/one-numeric-variable-random";
 import { useDimensions } from "../hook/use-dimensions";
-import { DensityChartBasic } from "../viz/DensityChartBasic/DensityChartBasic";
 import { AxisBasic } from "../viz/AxisBasic/AxisBasic";
 import { AxisBasicD3 } from "../viz/AxisBasicD3/AxisBasicD3";
+import { CodeBlock } from "../component/CodeBlock";
+import { TakeHome } from "../component/TakeHome";
 
 const graphDescription = (
   <p>
-    d3.js requires quite a lot of code to build a chart. But it allows to build
-    an axis very efficiently, thanks to a single function like{" "}
-    <code>axisLeft()</code> for instance.
+    This post explains how to build axes from d3 scales for a chart. It relies
+    on the <code>tick()</code> method to compute the tick positions and use
+    react for the rendering. The code of the <code>BottomAxis</code> and{" "}
+    <code>LeftAxis</code>
+    components is provided, together with some reproducible examples.
   </p>
 );
 
 const snippet1 = `
-export const useDimensions = (targetRef: React.RefObject<HTMLDivElement>) => {
+const TICK_LENGTH = 6;
 
-  const getDimensions = () => {
-    return {
-      width: targetRef.current ? targetRef.current.offsetWidth : 0,
-      height: targetRef.current ? targetRef.current.offsetHeight : 0
-    };
-  };
+export const AxisBottom = ({ xScale, pixelsPerTick }) => {
+  const range = xScale.range();
 
-  const [dimensions, setDimensions] = useState(getDimensions);
+  const ticks = useMemo(() => {
+    const width = range[1] - range[0];
+    const numberOfTicksTarget = Math.floor(width / pixelsPerTick);
 
-  const handleResize = () => {
-    setDimensions(getDimensions());
-  };
+    return xScale.ticks(numberOfTicksTarget).map((value) => ({
+      value,
+      xOffset: xScale(value),
+    }));
+  }, [xScale]);
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  return (
+    <>
+      {/* Main horizontal line */}
+      <path
+        d={["M", range[0], 0, "L", range[1], 0].join(" ")}
+        fill="none"
+        stroke="currentColor"
+      />
 
-  useLayoutEffect(() => {
-    handleResize();
-  }, []);
-
-  return dimensions;
-}
+      {/* Ticks and labels */}
+      {ticks.map(({ value, xOffset }) => (
+        <g key={value} transform={\`translate(\${xOffset}, 0)\`}>
+          <line y2={TICK_LENGTH} stroke="currentColor" />
+          <text
+            key={value}
+            style={{
+              fontSize: "10px",
+              textAnchor: "middle",
+              transform: "translateY(20px)",
+            }}
+          >
+            {value}
+          </text>
+        </g>
+      ))}
+    </>
+  );
+};
 `.trim();
 
 const snippet2 = `
-const chartRef = useRef(null);
+const TICK_LENGTH = 6;
+
+export const AxisLeft = ({ yScale, pixelsPerTick }) => {
+  const range = yScale.range();
+
+  const ticks = useMemo(() => {
+    const height = range[0] - range[1];
+    const numberOfTicksTarget = Math.floor(height / pixelsPerTick);
+
+    return yScale.ticks(numberOfTicksTarget).map((value) => ({
+      value,
+      yOffset: yScale(value),
+    }));
+  }, [yScale]);
+
+  return (
+    <>
+      {/* Main vertical line */}
+      <path
+        d={["M", 0, range[0], "L", 0, range[1]].join(" ")}
+        fill="none"
+        stroke="currentColor"
+      />
+
+      {/* Ticks and labels */}
+      {ticks.map(({ value, yOffset }) => (
+        <g key={value} transform={\`translate(0, \${yOffset})\`}>
+          <line x2={-TICK_LENGTH} stroke="currentColor" />
+          <text
+            key={value}
+            style={{
+              fontSize: "10px",
+              textAnchor: "middle",
+              transform: "translateX(-20px)",
+            }}
+          >
+            {value}
+          </text>
+        </g>
+      ))}
+    </>
+  );
+};
 `.trim();
 
 const snippet3 = `
@@ -72,15 +133,15 @@ export default function Home() {
 
   return (
     <Layout
-      title="How to build an axis for your react chart"
-      seoDescription="How to build a responsive chart with React and d3.js"
+      title="Building graph axes with React (and d3.js)"
+      seoDescription="This blogpost explains how to build axes from d3 scales for a chart. It relies on the tick() method to compute the tick positions and use react for the rendering."
     >
       <TitleAndDescription
-        title="How to build an axis for your react chart"
+        title="Building graph axes with React (and d3.js)"
         description={graphDescription}
       />
 
-      {/* Demo density chart */}
+      {/* Demo basic axis */}
       <div className="w-full flex flex-col justify-center items-center">
         <div
           style={{ height: 250, width: "100%", maxWidth: 500 }}
@@ -88,37 +149,77 @@ export default function Home() {
         >
           <AxisBasic width={axisDemo.width} height={axisDemo.height} />
         </div>
-        <p className="text-sm text-gray-500 max-w-xs italic text-center mt-4 font-light">
+        <p className="text-sm text-gray-500 max-w-md italic text-center mt-4 font-light">
           This minimal example uses <code>scaleLinear()</code> to compute the
-          scales, and react to render the axis.
+          scales, <code>ticks()</code> to compute tick positions and react to
+          render the axes.
         </p>
       </div>
 
       <AccordionSection
-        title={"D3.js and react axis: vocabulary"}
+        title={"Building a bottom axis with React"}
         startOpen={true}
       >
-        <p>Important to understand that a chart is composed by:</p>
-        <ul>
+        <p>
+          The code snippet below builds a <code>AxisBottom</code> component. It
+          is{" "}
+          <TakeHome>
+            very much inspired from{" "}
+            <a href="https://wattenberger.com/blog/react-and-d3">
+              this blogpost
+            </a>{" "}
+            by Amelia Wattenberger
+          </TakeHome>
+          . I've just changed a few things, notably passing a scale as input
+          instead of a range and a domain.
+        </p>
+        <p>
+          The logic mainly relies on the <code>ticks()</code> method of a d3
+          scale. It takes a target number of ticks as input, find the most
+          appropriate way to build smart ticks based on this target, and returns
+          an array with all the tick positions.
+        </p>
+        <p>
+          What follows is just some svg drawings based on those tick positions.
+        </p>
+        <CodeBlock code={snippet1} />
+      </AccordionSection>
+
+      <AccordionSection
+        title={"Building a left axis with React"}
+        startOpen={false}
+      >
+        <p>Exactly the same idea than for the bottom axis above</p>
+        <CodeBlock code={snippet2} />
+      </AccordionSection>
+
+      <AccordionSection title={"Dealing with margins"} startOpen={true}>
+        <p>
+          The bottom and left axes are not displays at the border of the main
+          chart component. Some margins are computed by the viz component. It is
+          important to understand that a chart is composed by:
+        </p>
+        <ul className="list-disc list-inside pl-10">
           <li>
-            <p>
-              - the global chart area, often specified by the <code>width</code>{" "}
-              and <code>height</code> properties of the chart components.
-            </p>
+            the global chart area, often specified by the <code>width</code> and{" "}
+            <code>height</code> properties of the chart components.
           </li>
           <li>
-            <p>
-              - the "bounds" area, i.e. the area located inside the x and y
-              axis. It is calculated by substracting the margins
-            </p>
+            the "bounds" area, i.e. the area located inside the x and y axis. It
+            is calculated by substracting the margins
           </li>
         </ul>
       </AccordionSection>
 
-      <AccordionSection title={"The react way"} startOpen={true}>
+      <AccordionSection title={"Use the axes"} startOpen={true}>
         <p>
-          Use d3.js to create the scale. And then react to render the segments
-          and numbers.
+          Once you have the bottom and left axis component described above you
+          just need to call them properly. You need to compute the bounds area
+          by substracting the margins to the total svg area.
+        </p>
+        <p>
+          Don't forget to add an additional translation to the bottom axis to
+          render it... at the bottom.
         </p>
         <ChartOrSandbox
           vizName={"AxisBasic"}
@@ -128,10 +229,14 @@ export default function Home() {
         />
       </AccordionSection>
 
-      <AccordionSection title={"The d3 way"} startOpen={true}>
+      <AccordionSection title={"Alternative: the d3 way"} startOpen={true}>
         <p>
-          Use d3.js to create the scale. And d3.js to render it using useEffect.
+          If you're a d3.js afficionados and want to deal with as little react
+          as possible, you can still use the good old <code>axisBottom()</code>{" "}
+          and <code>axisLeft()</code> methods of d3 and wrap them in a
+          <code>useEffect()</code> hook.
         </p>
+        <p>Here is an example below:</p>
         <ChartOrSandbox
           vizName={"AxisBasicD3"}
           maxWidth={600}
