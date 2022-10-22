44c87dbbb178d9c70b7fed63ebf2ff6bad470e6f
holtzy <yan.holtz@datadoghq.com>
2022-10-22 16:18:47 +0200

    add particles js test 1

---
+++ component/Particles.tsx
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
+export const ParticlesBg = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={}
+    />
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Contact from "../component/Contact";
 import SectionLogo from "../component/SectionLogo";
 import Link from "next/link";
 import { TypingAnimation } from "../component/TypingAnimation";
+import { ParticlesBg } from "../component/Particles";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       title="React Graph Gallery"
       seoDescription="The React Graph Gallery displays hundreds of charts made with React, always with explanation and reproduciible code"
     >
+      <ParticlesBg />
       <TitleAndDescription
         title={
           <span>
@@ -20,34 +22,30 @@ export default function Home() {
           </span>
         }
         description={
-          <p className="text-gray-400">
-            <i>
-              The react graph gallery displays chart examples built with{" "}
-              <b>React</b> and <b>d3.js</b>
-            </i>
-          </p>
+          <>
+            <p>
+              Have you ever wanted to create your <b>own chart</b>?
+            </p>
+            <p>
+              Based on <del>hundreds</del> (not yet) of graph examples, this
+              gallery guides you through the basic concepts of{" "}
+              <b>data visualization</b> with{" "}
+              <a href="https://reactjs.org">React</a> and{" "}
+              <a href="https://www.d3-graph-gallery.com">D3.js</a>. It also
+              provides ready-to-use <b>templates</b> to get started quicker.
+            </p>
+            <p>
+              Stop using pre-made dataviz components and <b>imagination</b> will
+              become the only boundary to your <b>creativity</b>.
+            </p>
+            <p className="mt-8">
+              The gallery is organized by chart types following the{" "}
+              <a href="https://www.data-to-viz.com">data-to-viz</a>{" "}
+              classification. What kind of graph are you interested in?
+            </p>
+          </>
         }
       />
-
-      <p>
-        Have you ever wanted to create your <b>own chart</b>?
-      </p>
-      <p>
-        Based on <del>hundreds</del> (not yet) of graph examples, this gallery
-        guides you through the basic concepts of <b>data visualization</b> with{" "}
-        <a href="https://reactjs.org">React</a> and{" "}
-        <a href="https://www.d3-graph-gallery.com">D3.js</a>. It also provides
-        ready-to-use <b>templates</b> to get started quicker.
-      </p>
-      <p>
-        Stop using pre-made dataviz components and <b>imagination</b> will
-        become the only boundary to your <b>creativity</b>.
-      </p>
-      <p className="mt-8">
-        The gallery is organized by chart types following the{" "}
-        <a href="https://www.data-to-viz.com">data-to-viz</a> classification.
-        What kind of graph are you interested in?
-      </p>
 
       <div className="mb-20">
         <ChartFamilySection chartFamily="distribution" />
