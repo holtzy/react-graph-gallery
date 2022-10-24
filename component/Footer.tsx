b6694461c13ac943d892b336becc4a2e48d86bb4
holtzy <yan.holtz@datadoghq.com>
2022-10-24 12:34:40 +0200

    better footer

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -5,17 +5,18 @@ import SocialMediaButtons from "./SocialMediaButtons";
 export default function Footer() {
   return (
     <div className="h-24 w-full grid grid-cols-3 gap-1 ">
-      <div className="text-sm font-light flex flex-row justify-start items-center">
-        <p className="hidden sm:inline">
-          Copyright © the React Graph Gallery 2021
-        </p>
+      <div className="text-sm font-light flex flex-row justify-start items-center mt-1">
+        <span className="hidden sm:inline">
+          Copyright © the <span className="gradient">React</span> Graph Gallery
+          2022
+        </span>
       </div>
 
       <div className="flex flex-row justify-center items-center ">
         <SocialMediaButtons />
       </div>
 
-      <div className="text-sm font-light flex flex-row justify-end items-center">
+      <div className="text-sm font-light flex flex-row justify-end items-center mt-1">
         <span className="hidden sm:inline">
           <span>About</span>
           <span> | </span>
