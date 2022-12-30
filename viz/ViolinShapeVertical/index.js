134997bcba614d0c31c18e94c9bb11e1bafeaee8
holtzy <yan.holtz@datadoghq.com>
2022-12-30 22:10:29 +0100

    fix index.js

--- viz/ViolinShapeVertical/index.js
+++ viz/ViolinShapeVertical/index.js
@@ -1,6 +1,23 @@
 import ReactDOM from "react-dom";
-import { data } from "./data";
-import { ViolinBasic } from "./ViolinBasic";
+import { VerticalViolinShape } from "./VerticalViolinShape";
+import * as d3 from "d3";
+
+const data = [
+  -1, 0, 0, 4, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 2, 3, 4, 3, 2, 3, 10,
+  3, 5, 5, 6, 4, 3, 5, 6, 6, 9, 9, 1, 9, 9, 8, 9, 9, 10, 6, 4, 3, 4, 3, 4, 3,
+  10, 11,
+];
+
+const yScale = d3.scaleLinear().domain([-2, 12]).range([400, 0]);
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<ViolinBasic data={data} width={400} height={400} />, rootElement);
+
+ReactDOM.render(
+  <VerticalViolinShape
+    width={400}
+    yScale={yScale}
+    data={data}
+    binNumber={10}
+  />,
+  rootElement
+);
