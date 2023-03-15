28dfe66c72c24158d6438acecba860c32086b2d8
holtzy <yan.holtz@datadoghq.com>
2023-03-15 14:36:25 +0100

    fix sandboxes

--- viz/LollipopDumbbell/index.js
+++ viz/LollipopDumbbell/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import { data } from "./data/";
-import { Lollipop } from "./Lollipop";
+import { Dumbbell } from "./Dumbbell";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Lollipop data={data} width={400} height={400} />, rootElement);
+ReactDOM.render(<Dumbbell data={data} width={400} height={400} />, rootElement);
