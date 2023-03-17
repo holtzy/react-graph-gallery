23e6efed5345f756e47d47e7f0415d0798fb719b
holtzy <yan.holtz@datadoghq.com>
2023-03-17 17:38:17 +0100

    ship wordcloud

--- pages/wordcloud.tsx
+++ pages/wordcloud.tsx
@@ -1,72 +1,32 @@
 import React from "react";
 import { Layout } from "../component/Layout";
 import TitleAndDescription from "../component/TitleAndDescription";
-import Contact from "../component/Contact";
 import ChartFamilySection from "../component/ChartFamilySection";
-import { AccordionSection } from "../component/AccordionSection";
 import { CodeBlock } from "../component/UI/CodeBlock";
 import { ChartOrSandbox } from "../component/ChartOrSandbox";
-import { HeatmapBasicDemo } from "../viz/HeatmapBasic/HeatmapBasicDemo";
-import { HeatmapVaccinationDemo } from "../viz/HeatmapVaccination/HeatmapVaccinationDemo";
-import { HeatmapTooltipDemo } from "../viz/HeatmapTooltip/HeatmapHeatmapTooltipDemo";
-import { TakeHome } from "../component/TakeHome";
 import { WordcloudBasicDemo } from "../viz/WordcloudBasic/WordCloudBasic";
+import Link from "next/link";
 
 const graphDescription = (
-  <p>
-    This page explains how to build a{" "}
-    <a href="https://www.data-to-viz.com/graph/wordcloud.html">wordcloud</a>{" "}
-    using
-    <code>react</code> and <code>d3.js</code>. It uses the{" "}
-    <a href="https://github.com/jasondavies/d3-cloud">d3-cloud</a> plugin to
-    compute the position of each word, and render them with react.
-  </p>
+  <>
+    <p>
+      This page explains how to build a{" "}
+      <a href="https://www.data-to-viz.com/graph/wordcloud.html">wordcloud</a>{" "}
+      using <code>react</code> and <code>d3.js</code>. It uses the{" "}
+      <a href="https://github.com/jasondavies/d3-cloud">d3-cloud</a> plugin to
+      compute the position of each word, and render them with react.
+    </p>
+    <p>
+      This section is rather short as{" "}
+      <a href="https://www.data-to-viz.com/graph/wordcloud.html">
+        I'm not a big fan
+      </a>{" "}
+      of wordclouds. They can be quite misleading and you should consider
+      building a <Link href="/barplot">barplot</Link> or a{" "}
+      <Link href="/lollipop">lollipop plot</Link> instead.
+    </p>
+  </>
 );
-
-const snippet1 = `
-const data = [
-  { text: "hello", value: 12 },
-  { text: "world", value: 2 },
-];
-`.trim();
-
-const snippet2 = `
-const xScale = useMemo(() => {
-  return d3
-    .scaleBand()
-    .range([0, boundsWidth])
-    .domain(allXGroups)
-    .padding(0.01);
-}, [data, width]);
-`.trim();
-
-const snippet3 = `
-// create a ref for the div that wrapps the viz:
-const chartRef = useRef(null);
-
-// The hook will check the dimension of the targeted div:
-const chartSize = useDimensions(chartRef);
-
-// chartSize is an object with the dimensions we need to pass to our viz component:
-return(
-  <div ref={chartRef}>
-    <MyChartComponent
-      height={chartSize.height}
-      width={chartSize.width}
-  </div>
-)
-`.trim();
-
-const snippet4 = `
-<div style={{ position: "relative" }}>
-  <Renderer ..someProps />
-  <Tooltip ..someProps />
-</div>
-`.trim();
-
-const snippet5 = `
-const [hoveredCell, setHoveredCell] = useState<HoveredCell | null>(null);
-`.trim();
 
 export default function Home() {
   return (
@@ -79,56 +39,123 @@ export default function Home() {
         description={graphDescription}
         chartType="wordcloud"
       />
-
       <blockquote>
         This page uses the{" "}
         <a href="https://github.com/jasondavies/d3-cloud">d3-cloud</a> plugin
         that you can install in your project with{" "}
         <code>npm install d3-cloud</code>
       </blockquote>
-
-      <AccordionSection title={"Dataset"} startOpen={true}>
-        <p>Pretty simple, just a set of words?</p>
-        <CodeBlock code={snippet1} />
-      </AccordionSection>
-
-      <AccordionSection
-        title={"Most basic wordcloud with React and D3.js"}
-        startOpen={true}
-      >
-        <p>
-          The process used to build a heatmap with react is very close from
-          building it with d3.js only. (See the pure d3 implementation{" "}
-          <a href="https://d3-graph-gallery.com/heatmap">here</a>).
-        </p>
-
-        <h3>&rarr; Data wrangling</h3>
-        <p>
-          Before building scales we need an exhaustive list of the X and Y axis
-          steps. We also need to compute the min and max of the `value` property
-          of the dataset to compute the color scale.
-        </p>
-        <p>
-          As always, don't forget to wrap that kind of work in a{" "}
-          <code>useMemo</code>. You want to compute it only when the{" "}
-          <code>data</code> changes.
-        </p>
-
-        <br />
-        <ChartOrSandbox
-          VizComponent={WordcloudBasicDemo}
-          vizName={"WordcloudBasic"}
-          maxWidth={600}
-          height={300}
-          caption={"Most basic Wordcloud made with react and d3.js"}
-        />
-      </AccordionSection>
-
-      <div className="full-bleed border-t h-0 bg-gray-100 my-3" />
+      {/*
+      //
+      // Data
+      //
+      */}
+      <h2 id="data">The Data</h2>
+      <p>
+        The data is an <b>array</b>. Each item is an object describing a word.
+        Its <code>name</code> is provided, together with a related{" "}
+        <code>value</code> that will be used to size the word on the final
+        figure.
+      </p>
+      <p>
+        Note that you can add <b>any additional property</b> here, like a color,
+        a font weight or anything else that you want to use to draw the word
+        later on.
+      </p>
+      <CodeBlock code={snippetData} />
+      {/*
+      //
+      // Basic
+      //
+      */}
+      <h2 id="basic">Most basic wordcloud with React and D3.js</h2>
+      <p>
+        Everything starts by instantiating a wordcloud layout using the
+        <code>d3Cloud()</code> function of the{" "}
+        <a href="https://github.com/jasondavies/d3-cloud">d3-cloud</a> library.
+      </p>
+      <CodeBlock code={snippetLayout} />
+      <p>
+        This layout can then be called from a useEffect using{" "}
+        <code>layout.start()</code>. The layout algorithm will loop through each
+        word of the dataset and try to place them on the chart, avoiding
+        overlaps with other words.
+      </p>
+      <p>
+        Once the loop is over, the layout algorithm will produce a{" "}
+        <code>words</code> object and provide it to the <code>end()</code>{" "}
+        function. This function update a state that stores the position and
+        feature of each word.
+      </p>
+      <p>
+        It is thus possible to map through those word features and draw them
+        using html, svg or canvas. Here is an example using HTML
+      </p>
+      <br />
+      <ChartOrSandbox
+        VizComponent={WordcloudBasicDemo}
+        vizName={"WordcloudBasic"}
+        maxWidth={600}
+        height={500}
+        caption={"Most basic Wordcloud made with react and d3.js"}
+      />
+      <blockquote>
+        <b>Todo</b>: write better explanation
+      </blockquote>
+      <div className="mt-4" />
+      <blockquote>
+        <b>Todo</b>: the layout algorithm currently provides unperfect values,
+        resulting in a lot of word overlaps. Please tell me if you find where
+        the bug is.
+      </blockquote>
+      {/*
+      //
+      // Warning
+      //
+      */}
+      <h2 id="warning">Warning</h2>
+      <p>
+        Wordclouds are useful for quickly perceiving the most prominent terms.
+        They are widely used in media and well understood by the public.
+        However, they are criticized for 2 main reasons:
+      </p>
+      <ul>
+        <li>
+          Area is a poor metaphor of a numeric value, it is hardly perceive by
+          the human eye
+        </li>
+        <li>Longer words appear bigger by construction</li>
+      </ul>
+      <p>
+        To put it in a nutshell, wordclouds must be avoided. You can read more
+        about that in{" "}
+        <a href="https://www.data-to-viz.com/graph/wordcloud.html">
+          data-to-viz
+        </a>
+        . Why not consider a <Link href="/lollipop">lollipop plot</Link> or a{" "}
+        <Link href="/barplot">barplot</Link> instead?
+      </p>
+      <div className="full-bleed border-t h-0 bg-gray-100 my-3 mt-28" />
       <ChartFamilySection chartFamily="ranking" />
-
       <div className="mt-20" />
-      <Contact />
     </Layout>
   );
 }
+
+const snippetData = `
+const data = [
+  { text: "hello", value: 12 },
+  { text: "world", value: 2 },
+];
+`.trim();
+
+const snippetLayout = `
+const layout = d3Cloud()
+  .words(data)
+  .size([width, height])
+  .fontSize((d) => fontSizeScale(d.value))
+  .padding(10)
+  .on("end", (words) => {
+    setWordsPosition(words);
+  });
+`.trim();
--- viz/WordcloudBasic/Wordcloud.tsx
+++ viz/WordcloudBasic/Wordcloud.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import d3Cloud from "d3-cloud";
-import { select } from "d3";
+import { interpolateInferno, scaleLinear, scaleSequential } from "d3";
 
 interface Datum {
   text: string;
@@ -19,43 +19,51 @@ export interface Word extends d3Cloud.Word {
 }
 
 export const Wordcloud = ({ width, height, data }: WordcloudProps) => {
-  const ref = useRef(null);
+  const [wordsPosition, setWordsPosition] = useState<Word[]>([]);
+
+  const fontSizeScale = scaleLinear().domain([0, 100]).range([12, 35]);
+  const colorScale = scaleSequential()
+    .domain([0, 100])
+    .interpolator(interpolateInferno);
+
+  const layout = d3Cloud<Word>()
+    .words(data)
+    .timeInterval(100)
+    .size([width, height])
+    .fontSize((d) => fontSizeScale(d.value))
+    .padding(10)
+    .on("end", (words) => {
+      setWordsPosition(words);
+    });
 
   useEffect(() => {
-    if (!ref.current) {
+    if (!width) {
       return;
     }
-
-    const el = ref.current;
-
-    // clear old words
-    select(el).selectAll("*").remove();
-
-    const layout = d3Cloud<Word>()
-      .words(data)
-      .size([width, height])
-      .on("end", (words) => {
-        const [w, h] = layout.size();
-        select(el)
-          .append("svg")
-          .attr("viewBox", `0 0 ${w} ${h}`)
-          .attr("preserveAspectRatio", "xMinYMin meet")
-          .append("g")
-          .attr("transform", `translate(${w / 2},${h / 2})`)
-          .selectAll("text")
-          .data(words)
-          .enter()
-          .append("text")
-          .attr("text-anchor", "middle")
-          .attr(
-            "transform",
-            (d) => `translate(${[d.x, d.y]})rotate(${d.rotate})`
-          )
-          .text((d) => d.text);
-      });
-
     layout.start();
-  }, [width, height, data]);
+  }, [width]);
 
-  return <div style={{ width, height }} ref={ref} />;
+  return (
+    <div style={{ width, height, position: "relative" }}>
+      <div style={{ transform: `translate(${width / 2}px,${height / 2}px)` }}>
+        {wordsPosition.map((word, i) => (
+          <p
+            key={i}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              fontSize: word.size,
+              textAnchor: "middle",
+              transform: `translate(${word.x}px,${word.y}px)rotate(${word.rotate}deg)`,
+              color: colorScale(word.value),
+              font: "serif",
+            }}
+          >
+            {word.text}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
