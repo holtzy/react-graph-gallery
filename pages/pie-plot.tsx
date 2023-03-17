22cd3e52932959fd02a6115d901d209395a69e5e
holtzy <yan.holtz@datadoghq.com>
2023-03-17 21:56:43 +0100

    start pie

--- pages/pie-plot.tsx
+++ pages/pie-plot.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { Layout } from "../component/Layout";
 import TitleAndDescription from "../component/TitleAndDescription";
-import Contact from "../component/Contact";
 import { ChartOrSandbox } from "../component/ChartOrSandbox";
 import ChartFamilySection from "../component/ChartFamilySection";
 import { AccordionSection } from "../component/AccordionSection";
 import { CodeBlock } from "../component/UI/CodeBlock";
-import { data } from "../data/one-value-per-group-random";
 import { PieChartBasicDemo } from "../viz/PieChartBasic/PieChartBasicDemo";
 
 const graphDescription = (
@@ -18,7 +16,148 @@ const graphDescription = (
   </p>
 );
 
-const snippet1 = `
+export default function Home() {
+  return (
+    <Layout
+      title="Pie chart with React"
+      seoDescription="How to build a pie chart with React and D3.js. A set of re-usable components"
+    >
+      <TitleAndDescription
+        title="Pie chart"
+        description={graphDescription}
+        chartType="pie"
+      />
+      {/*
+      //
+      // Data
+      //
+      */}
+      <h2 id="data">The Data</h2>{" "}
+      <p>
+        The dataset required to build a pie chart is pretty simple. It is an
+        array where each item represents a group of the pie chart. Each item is
+        an object with 2 properties. They provide the group name (
+        <code>name</code>) and its value (<code>value</code>).
+      </p>
+      <br />
+      <p>
+        For instance, here is the dataset used for the simple pie chart below:
+      </p>
+      <CodeBlock code={snippetData} />
+      {/*
+      //
+      // Skeleton
+      //
+      */}
+      <h2 id="skeleton">Component skeleton</h2>
+      <p>
+        The goal here is to create a <code>PieChart</code> component that will
+        be stored in a <code>PieChart.tsx</code> file. This component requires 3
+        props to render: a <code>width</code>, a <code>height</code>, and some{" "}
+        <code>data</code>.
+      </p>
+      <p>
+        The shape of the <code>data</code> is described above. The{" "}
+        <code>width</code> and <code>height</code> will be used to render an{" "}
+        <code>svg</code> element in the DOM, in which we will insert the pie
+        chart.
+      </p>
+      <p>
+        To put it in a nutshell, that's the skeleton of our{" "}
+        <code>PieChart</code> component:
+      </p>
+      <CodeBlock code={snippetSkeleton} />
+      <p>
+        It's fundamental to understand that with this code organization, d3.js
+        will be used to prepare the SVG arcs, but it's React that will render
+        them in the <code>return()</code> statement. We won't use d3 methods
+        like <code>append</code> that you can find in usual{" "}
+        <a href="https://www.d3-graph-gallery.com">d3.js examples</a>.
+      </p>
+      {/*
+      //
+      // Data to arcs
+      //
+      */}
+      <h2 id="build arcs">From data to arcs</h2>
+      <p>
+        As usual, the math is done thanks to d3.js, and the rendering using
+        React
+      </p>
+      <h3>&rarr; A pie chart is a set of arcs</h3>
+      <p>Explain the concept of arcs</p>
+      <h3>
+        &rarr; <code>d3.pie()</code> to compute arc details
+      </h3>
+      <p>
+        First of all, the <code>pie()</code> function is used to compute the
+        start and end angles of each group. By default this function expects an
+        array of number as input. Thus we have to tell it how to find the value
+        to use in our dataset using an assesor function
+      </p>
+      <CodeBlock code={snippet2} />
+      <h3>
+        &rarr; <code>d3.arc()</code> to compute the svg path
+      </h3>
+      <p>
+        From those start and end angles we now need to build a proper string
+        that we can pass as the <code>d</code> attribute of a <code>path</code>.
+        This is pretty easy thanks to the <code>arc()</code> function of d3.
+        This function must be applied to every item of the pie object created
+        above.
+      </p>
+      <CodeBlock code={snippet3} />
+      {/*
+      //
+      // Rendering
+      //
+      */}
+      <h2 id="basic pie">Basic pie chart</h2>
+      <p>
+        And that's it. This array of path can be renderer using react using a
+        map pretty easily.
+      </p>
+      <ChartOrSandbox
+        vizName={"PieChartBasic"}
+        VizComponent={PieChartBasicDemo}
+        maxWidth={400}
+        height={400}
+        caption="basic pie chart with react and d3.js"
+      />
+      {/*
+      //
+      // Warning
+      //
+      */}
+      <h2 id="warning">Warning</h2>
+      {/*
+      //
+      // Legend
+      //
+      */}
+      <h2 id="legend">Legend</h2>
+      <p>Copy donut. Explain it must be inline</p>
+      {/*
+      //
+      // Hover effect
+      //
+      */}
+      <h2 id="hover effect">Hover effect</h2>
+      {/*
+      //
+      // Data transition
+      //
+      */}
+      <h2 id="data transition">Data transition</h2>
+      <p>Copy donut. Explain it must be inline</p>
+      <div className="full-bleed border-t h-0 bg-gray-100 my-3" />
+      <ChartFamilySection chartFamily="partOfAWhole" />
+      <div className="mt-20" />
+    </Layout>
+  );
+}
+
+const snippetData = `
 const data = [
   {name:"Mark", value: 90},
   {name:"Robert", value: 12},
@@ -26,6 +165,31 @@ const data = [
   {name:"Marion", value: 53},
   {name:"Nicolas", value: 98},
 ]
+`.trim();
+
+const snippetSkeleton = `
+import * as d3 from "d3"; // we will need d3.js
+
+type PieChartProps = {
+  width: number;
+  height: number;
+  data: {name: string, value: number}[];
+};
+
+export const PieChart = ({ width, height, data }: PieChartProps) => {
+
+  // read the data
+  // generate position of each slice with d3.pie()
+  // build the arcs
+
+  return (
+    <div>
+      <svg width={width} height={height}>
+        // render all the arcs
+      </svg>
+    </div>
+  );
+};
 `.trim();
 
 const snippet2 = `
@@ -64,79 +228,3 @@ const arcs pie.map((p) =>
 ]
 */
 `.trim();
-
-export default function Home() {
-  const [animData, setAnimData] = useState(data);
-
-  return (
-    <Layout
-      title="Pie chart with React"
-      seoDescription="How to build a pie chart with React and D3.js. A set of re-usable components"
-    >
-      <TitleAndDescription
-        title="Pie chart"
-        description={graphDescription}
-        chartType="pie"
-      />
-
-      <AccordionSection title={"Dataset"} startOpen={true}>
-        <p>
-          The dataset required to build a pie chart is pretty simple. It is an
-          array where each item represents a group of the pie chart. Each item
-          is an object with 2 properties. They provide the group name (
-          <code>name</code>) and its value (<code>value</code>).
-        </p>
-        <br />
-        <p>
-          For instance, here is the dataset used for the simple pie chart below:
-        </p>
-        <CodeBlock code={snippet1} />
-      </AccordionSection>
-
-      <AccordionSection title={"Most basic pie chart"} startOpen={true}>
-        <p>
-          As usual, the math is done thanks to d3.js, and the rendering using
-          React
-        </p>
-        <br />
-        <p>
-          First of all, the <code>pie()</code> function is used to compute the
-          start and end angles of each group. By default this function expects
-          an array of number as input. Thus we have to tell it how to find the
-          value to use in our dataset using an assesor function
-        </p>
-        <CodeBlock code={snippet2} />
-        <br />
-        <p>
-          From those start and end angles we now need to build a proper string
-          that we can pass as the <code>d</code> attribute of a{" "}
-          <code>path</code>. This is pretty easy thanks to the{" "}
-          <code>arc()</code> function of d3. This function must be applied to
-          every item of the pie object created above.
-        </p>
-        <CodeBlock code={snippet3} />
-        <br />
-        <p>
-          And that's it. This array of path can be renderer using react using a
-          map pretty easily.
-        </p>
-        <br />
-        <br />
-
-        <ChartOrSandbox
-          vizName={"PieChartBasic"}
-          VizComponent={PieChartBasicDemo}
-          maxWidth={400}
-          height={400}
-          caption="basic pie chart with react and d3.js"
-        />
-      </AccordionSection>
-
-      <div className="full-bleed border-t h-0 bg-gray-100 my-3" />
-      <ChartFamilySection chartFamily="partOfAWhole" />
-
-      <div className="mt-20" />
-      <Contact />
-    </Layout>
-  );
-}
