cf39dcb22b902fd48197bd12baaffd02bec8dec1
holtzy <yan.holtz@datadoghq.com>
2023-06-06 10:05:49 +0200

    fix index of sandbox

--- viz/LineChartSyncCursor/LineChartSyncCursorDemo.tsx
+++ viz/LineChartSyncCursor/LineChartSyncCursorDemo.tsx
@@ -1,28 +1,5 @@
-import { useState } from "react";
-import { data, data2 } from "./data";
-import { LineChart } from "./LineChart";
+import { LineChartSyncCursor } from "./LineChartSyncCursor";
 
 export const LineChartSyncCursorDemo = ({ width = 700, height = 400 }) => {
-  const [cursorPosition, setCursorPosition] = useState<number | null>(null);
-
-  return (
-    <div style={{ display: "flex" }}>
-      <LineChart
-        data={data}
-        width={width / 2}
-        height={height}
-        cursorPosition={cursorPosition}
-        setCursorPosition={setCursorPosition}
-        color={"#e85252"}
-      />
-      <LineChart
-        data={data2}
-        width={width / 2}
-        height={height}
-        cursorPosition={cursorPosition}
-        setCursorPosition={setCursorPosition}
-        color={"#6689c6"}
-      />
-    </div>
-  );
+  return <LineChartSyncCursor width={width} height={height} />;
 };
