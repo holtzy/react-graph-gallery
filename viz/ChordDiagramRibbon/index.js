08b201c65c70f0bde59f34c25e4b6af140d579d9
holtzy <yan.holtz@datadoghq.com>
2023-06-13 14:31:14 +0200

    fix chord sandboxes

--- viz/ChordDiagramRibbon/index.js
+++ viz/ChordDiagramRibbon/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom";
 import { data } from "./data/";
-import { AreaChart } from "./AreaChart";
+import { ChordDiagram } from "./ChordDiagram";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <AreaChart data={data} width={400} height={400} />,
+  <ChordDiagram data={data} width={400} height={400} />,
   rootElement
 );
