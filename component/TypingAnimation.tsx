7d799128b4032c16c2b0303df824758a543d4d35
Holtz Yan <holtzy@Holtzs-MacBook-Pro.local>
2023-07-12 18:02:40 +0200

    fix type and bug of header

--- component/TypingAnimation.tsx
+++ component/TypingAnimation.tsx
@@ -1,12 +1,12 @@
-import { type } from "os";
-import React, { useEffect, useRef, useState } from "react";
+import { type } from 'os';
+import React, { useEffect, useRef, useState } from 'react';
 
-const wordList = ["unique", "performant", "interactive", "insightful"];
+const wordList = ['unique', 'performant', 'interactive', 'insightful'];
 const SPEED = 40; // time between 2 letters
 const PAUSE = 5000;
 
 export const TypingAnimation = () => {
-  const ref = useRef(null);
+  const ref = useRef<HTMLSpanElement>(null);
 
   let letterIndex = 0;
   let wordIndex = 0;
@@ -18,13 +18,19 @@ export const TypingAnimation = () => {
       return;
     }
 
+    // If some letters are still needed to finish the word
     if (letterIndex < word.length) {
       ref.current.innerHTML += word.charAt(letterIndex);
       letterIndex++;
       setTimeout(typeWriter, SPEED);
     } else {
+      // The word is over. Wait a bit before starting a new one.
       setTimeout(() => {
-        ref.current.innerHTML = "";
+        if (!ref?.current) {
+          return;
+        }
+
+        ref.current.innerHTML = '';
         letterIndex = 0;
 
         if (wordIndex >= wordList.length - 1) {
@@ -42,9 +48,9 @@ export const TypingAnimation = () => {
   });
 
   return (
-    <span style={{ width: 250, display: "inline-block" }}>
+    <span style={{ width: 250, display: 'inline-block' }}>
       <span ref={ref} className="gradient with-blinking-cursor">
-        {""}
+        {''}
       </span>
     </span>
   );
