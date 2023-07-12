08071064d0e401ba5f90fd40fa4d74f7ca5c0c3b
Holtz Yan <holtzy@Holtzs-MacBook-Pro.local>
2023-07-12 17:39:12 +0200

    fix sandboxes

--- viz/BackgroundMapProjection/index.js
+++ viz/BackgroundMapProjection/index.js
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom';
 import { MapPlayground } from './MapPlayground';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<MapPlayground width={width} height={height} />, rootElement);
+ReactDOM.render(<MapPlayground width={600} height={600} />, rootElement);
