4f47b5088c18df5c8e9deedf4029453792194dc5
Holtz Yan <holtzy@Holtzs-MacBook-Pro.local>
2023-07-21 14:00:18 +0200

    fix sandbox

--- viz/BubbleMapDatasetTransition/index.js
+++ viz/BubbleMapDatasetTransition/index.js
@@ -1,9 +1,7 @@
 import ReactDOM from 'react-dom';
-import { numData, geoData } from './data';
-import { Map } from './Map';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <Map geoData={geoData} numData={numData} width={700} height={400} />,
+  <BubbleMapDatasetTransition width={700} height={400} />,
   rootElement
 );
