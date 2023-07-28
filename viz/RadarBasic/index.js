8408afa69698adaa20c0e8bbfd7f42fdc6eb0cc0
Holtz Yan <holtzy@Holtzs-MacBook-Pro.local>
2023-07-28 11:04:16 +0200

    fix sanboxes

--- viz/RadarBasic/index.js
+++ viz/RadarBasic/index.js
@@ -1,14 +1,21 @@
-import ReactDOM from "react-dom";
-import { data } from "./data";
-import { ParallelCoordinate } from "./ParallelCoordinate";
+import ReactDOM from 'react-dom';
+import { data } from './data';
+import { Radar } from './Radar';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <ParallelCoordinate
+  <Radar
     data={data}
-    width={600}
-    height={500}
-    variables={["petalLength", "petalWidth", "sepalLength", "sepalWidth"]}
+    width={width}
+    height={height}
+    axisConfig={[
+      { name: 'speed', max: 10 },
+      { name: 'acceleration', max: 10 },
+      { name: 'conso', max: 10 },
+      { name: 'safety', max: 2 },
+      { name: 'style', max: 1000 },
+      { name: 'price', max: 100 },
+    ]}
   />,
   rootElement
 );
