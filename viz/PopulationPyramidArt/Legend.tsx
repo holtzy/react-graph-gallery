780e089d2d1e86b6378f1e4a0a28dd486a9b1814
Yan Holtz <yan.holtz.data@gmail.com>
2024-07-19 15:02:00 +0200

    interactive legend

--- viz/PopulationPyramidArt/Legend.tsx
+++ viz/PopulationPyramidArt/Legend.tsx
@@ -1,8 +1,10 @@
+import { useRef } from 'react';
 import { colorScale } from './utils';
+import styles from './legend.module.css';
 
 type LegendProps = {
   setHighlightedYear: (y: number | undefined) => void;
-  highlightedYear: number;
+  highlightedYear: number | undefined;
 };
 
 const start = 1950;
@@ -18,31 +20,49 @@ export const Legend = ({
   setHighlightedYear,
   highlightedYear,
 }: LegendProps) => {
+  const legendContainerRef = useRef(null);
+
   const allLines = steps.map((y) => {
     return (
-      <div className="flex items-center" style={{ height: 1 }}>
+      <div
+        className={'flex items-center h-2 ' + styles.yearItem}
+        onMouseOver={() => {
+          if (legendContainerRef.current) {
+            legendContainerRef.current.classList.add(styles.hasHighlight);
+          }
+          // setHighlightedYear(y);
+        }}
+        onMouseLeave={() => {
+          if (legendContainerRef.current) {
+            legendContainerRef.current.classList.remove(styles.hasHighlight);
+          }
+          // setHighlightedYear(undefined);
+        }}
+      >
         <div
           style={{ backgroundColor: colorScale(y) }}
-          className="w-full h-full"
-          onMouseOver={() => {
-            setHighlightedYear(y);
-          }}
-          onMouseLeave={() => {
-            setHighlightedYear(undefined);
-          }}
+          className="w-full h-1"
         />
 
-        <span className="text-xs text-white pl-2">{y}</span>
+        <span
+          className={
+            'text-xs text-white pl-2 pointer-events-none ' + styles.yearText
+          }
+        >
+          {y}
+        </span>
       </div>
     );
   });
 
   return (
-    <div className="relative flex flex-col w-12 " style={{ gap: 6 }}>
+    <div ref={legendContainerRef} className="relative flex flex-col w-12">
       {allLines}
+
       <div className="absolute top-0 left-0 transform -translate-x-full -translate-y-1/2">
         <span className="text-xs text-white pr-2">1950</span>
       </div>
+
       <div className="absolute bottom-0 left-0 transform -translate-x-full translate-y-1/2">
         <span className="text-xs text-white pr-2">2100</span>
       </div>
