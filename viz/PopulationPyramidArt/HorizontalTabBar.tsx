8c082c8397c99ee23d18bdd9716164e31e469576
Yan Holtz <yan.holtz.data@gmail.com>
2024-07-19 11:06:57 +0200

    get back changes

--- viz/PopulationPyramidArt/HorizontalTabBar.tsx
+++ viz/PopulationPyramidArt/HorizontalTabBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 type HorizontalTabBarProps = {
   selectedItem: number;
@@ -11,36 +11,64 @@ export const HorizontalTabBar = ({
   setSelectedItem,
   items,
 }: HorizontalTabBarProps) => {
+  const containerRef = useRef(null);
+  const buttonRefs = useRef<(HTMLButtonElement | null)[]>([]);
+
+  const scrollToButton = (index: number) => {
+    const targetRef = buttonRefs.current[index];
+    if (targetRef) {
+      targetRef.scrollIntoView({
+        behavior: 'smooth',
+        inline: 'center', // This ensures the element scrolls to the center
+      });
+    }
+  };
+
   useEffect(() => {
-    const handleKeyDown = (event) => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      let targetId = selectedItem;
+
       if (event.key === 'ArrowRight') {
-        setSelectedItem(selectedItem + 1);
+        targetId += 1;
       } else if (event.key === 'ArrowLeft') {
-        setSelectedItem(selectedItem - 1);
+        targetId -= 1;
       }
+
+      if (targetId < 0) {
+        targetId = items.length - 1;
+      }
+      if (targetId > items.length - 1) {
+        targetId = 0;
+      }
+
+      setSelectedItem(targetId);
+      scrollToButton(targetId);
     };
 
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [selectedItem]);
+  }, [selectedItem, setSelectedItem, items]);
 
   const groupSelectButtons = items.map((grp, i) => {
     return (
       <button
         key={i}
-        className={`text-xs px-2 py-1 bg-black border border-white text-white rounded-lg whitespace-nowrap flex-shrink-0`}
+        className={`text-xs px-2 py-1 bg-black border border-white text-white rounded-lg whitespace-nowrap flex-shrink-0 ${
+          i === selectedItem ? 'bg-red-300' : ''
+        }`}
         onClick={() => setSelectedItem(i)}
+        ref={(el) => (buttonRefs.current[i] = el)}
       >
-        {grp}
+        {getFlagEmoji('FR') + grp}
       </button>
     );
   });
 
   return (
     <div className="relative max-w-3xl">
-      <div className="overflow-scroll w-full flex gap-1">
+      <div ref={containerRef} className="overflow-scroll w-full flex gap-1">
         {groupSelectButtons}
       </div>
       <div className="absolute inset-y-0 left-0 w-28 h-full  bg-gradient-to-r from-black to-transparent"></div>
@@ -48,3 +76,9 @@ export const HorizontalTabBar = ({
     </div>
   );
 };
+
+export default function getFlagEmoji(countryCode: string) {
+  return [...countryCode.toUpperCase()]
+    .map((char) => String.fromCodePoint(127397 + char.charCodeAt(0)))
+    .reduce((a, b) => `${a}${b}`);
+}
--- viz/PopulationPyramidArt/PopulationPyramid.tsx
+++ viz/PopulationPyramidArt/PopulationPyramid.tsx
@@ -2,7 +2,7 @@ import * as d3 from 'd3';
 import { LineItem } from './LineItem';
 
 const COLORS = ['#e0ac2b', '#e85252', '#6689c6', '#9a6fb0', '#a53253'];
-const MARGIN = { top: 30, right: 30, bottom: 30, left: 30 };
+const MARGIN = { top: 30, right: 0, bottom: 30, left: 0 };
 
 type DataItem = {
   AgeGrpStart: string;
@@ -28,8 +28,6 @@ export const PopulationPyramid = ({
   const boundsWidth = width - MARGIN.right - MARGIN.left;
   const boundsHeight = height - MARGIN.top - MARGIN.bottom;
 
-  console.log('data', data);
-
   const dataFiltered = data.filter((d) => d.Location === selectedGroup);
 
   const allYears = [...new Set(data.map((d) => d.MidPeriod))].sort();
@@ -51,12 +49,12 @@ export const PopulationPyramid = ({
   // Males on the left
   const xScaleMale = d3
     .scaleLinear()
-    .range([0, boundsWidth / 2])
+    .range([0, boundsWidth / 2 - 10])
     .domain([10, 0]);
 
   const xScaleFemale = d3
     .scaleLinear()
-    .range([boundsWidth / 2, boundsWidth])
+    .range([boundsWidth / 2 + 10, boundsWidth])
     .domain([0, 10]);
 
   const lineBuilderMale = d3
@@ -96,8 +94,48 @@ export const PopulationPyramid = ({
   });
 
   return (
-    <div className="bg-black">
+    <div className="relative bg-black">
       <svg width={width} height={height}>
+        <text
+          x={width / 2}
+          y={15}
+          fill="white"
+          fontSize={10}
+          alignmentBaseline="central"
+          textAnchor="middle"
+        >
+          100 years old
+        </text>
+        <line
+          x1={width / 2 - 5}
+          y1={32}
+          x2={width / 2 + 5}
+          y2={32}
+          stroke="white"
+          opacity={1}
+          strokeWidth={0.5}
+        />
+
+        <text
+          x={width / 2}
+          y={height - 15}
+          fill="white"
+          fontSize={10}
+          alignmentBaseline="central"
+          textAnchor="middle"
+        >
+          0 years old
+        </text>
+        <line
+          x1={width / 2 - 5}
+          y1={height - MARGIN.top}
+          x2={width / 2 + 5}
+          y2={height - MARGIN.top}
+          stroke="white"
+          opacity={1}
+          strokeWidth={0.5}
+        />
+
         <g
           width={boundsWidth}
           height={boundsHeight}
