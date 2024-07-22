fd859fe65582bde342af6ac333d2103da9392b09
Yan Holtz <yan.holtz.data@gmail.com>
2024-07-22 14:34:57 +0200

    start explanation section

--- viz/PopulationPyramidArt/ExplanationSection.tsx
+++ viz/PopulationPyramidArt/ExplanationSection.tsx
@@ -7,12 +7,30 @@ type ExplanationSectionProps = {};
 
 const sections = ['section1', 'section2', 'section3'];
 
+const getExplanations = (id: number) => {
+  let text = null;
+  switch (id) {
+    case 1:
+      text = <p>Just an histogram</p>;
+      break;
+    case 2:
+      text = <p>With the lines mate!</p>;
+      break;
+    case 3:
+      text = 'State is 2';
+      break;
+    default:
+      text = null;
+  }
+  return text;
+};
+
 export const ExplanationSection = ({}: ExplanationSectionProps) => {
   const refs = useRef(sections.map(() => createRef()));
 
-  const [opacity, setOpacity] = useState(0);
+  const [step, setStep] = useState(1);
 
-  const [selectedData, setSelectedData] = useState(dummyData);
+  const [opacity, setOpacity] = useState(0);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -23,7 +41,6 @@ export const ExplanationSection = ({}: ExplanationSectionProps) => {
 
             if (entry.target.id === 'section2') {
               setOpacity(1);
-              setSelectedData(bahrainData);
             }
           }
         });
@@ -48,6 +65,8 @@ export const ExplanationSection = ({}: ExplanationSectionProps) => {
     };
   }, []);
 
+  const selectedData = step === 1 ? dummyData : bahrainData;
+
   return (
     <div className="text-white text-sm wrapper mt-24 flex flex-col justify-center items-start">
       <p className="text-gray-400 text-xl uppercase">Looks good but</p>
@@ -55,8 +74,45 @@ export const ExplanationSection = ({}: ExplanationSectionProps) => {
 
       <div className="h-96" ref={refs.current[0]} id={sections[0]}>
         <p>That's a population pyramid!</p>
-        <p>OK, not a very conventional population pyramid. But still!</p>
+
+        <div className="grid grid-cols-12">
+          {/* LEFT */}
+          <div className="col-span-8">
+            <p>OK, not a very conventional population pyramid. But still!</p>
+            <p>Let me show you step by step.</p>
+            <br></br>
+            <div className="flex gap-2">
+              <button
+                onClick={() => {
+                  setStep(step - 1);
+                }}
+              >
+                Previous step
+              </button>
+              <p>{'Step ' + step}</p>
+              <button
+                onClick={() => {
+                  setStep(step + 1);
+                }}
+              >
+                Next step
+              </button>
+            </div>
+            <div>{getExplanations(step)}</div>
+          </div>
+
+          {/* RIGHT */}
+          <div className="col-span-4">
+            <PopulationPyramid
+              data={selectedData}
+              width={300}
+              height={500}
+              highlightedYear={undefined}
+            />
+          </div>
+        </div>
       </div>
+
       <div className="h-96"></div>
 
       <div
@@ -66,18 +122,6 @@ export const ExplanationSection = ({}: ExplanationSectionProps) => {
         style={{ opacity }}
       >
         <p>that's the second section</p>
-        <p>OK, not a very conventional population pyramid. But still!</p>
-        <PopulationPyramid
-          data={selectedData}
-          width={300}
-          height={500}
-          highlightedYear={undefined}
-        />
-      </div>
-
-      <div className="h-96"></div>
-      <div className="h-96" ref={refs.current[2]} id={sections[2]}>
-        <p>And the third</p>
         <p>OK, not a very conventional population pyramid. But still!</p>
       </div>
     </div>
