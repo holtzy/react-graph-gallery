9a569f646a2937ca9c88370a8c1db0cb76e97c14
Yan Holtz <yan.holtz.data@gmail.com>
2024-08-08 11:44:07 +0200

    d

---
+++ viz/exercise/SvgPathFirstAreaChartSolution/Graph.tsx
@@ -0,0 +1,11 @@
+export const Graph = () => {
+  return (
+    <svg width={500} height={300} style={{ overflow: 'visible' }}>
+      <path
+        d="M0 40 L50 70 L100 150 L150 150 L200 200 L250 50 L300 90"
+        fill="none"
+        stroke="#69b3a2"
+      />
+    </svg>
+  );
+};
