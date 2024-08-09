9d888a4d7417864fee2f1010f27b7849e3092c0c
Yan Holtz <yan.holtz.data@gmail.com>
2024-08-09 09:36:31 +0200

    fix assets

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
