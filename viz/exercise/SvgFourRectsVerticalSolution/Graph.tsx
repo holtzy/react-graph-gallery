ce51ebd1edf8d6983823aa84e77ffcbe6799532b
Holtz Yan <yan.holtz@datadoghq.com>
2024-10-22 17:17:06 +0200

    x

--- viz/exercise/SvgFourRectsVerticalSolution/Graph.tsx
+++ viz/exercise/SvgFourRectsVerticalSolution/Graph.tsx
@@ -3,29 +3,29 @@ export const Graph = () => {
     <svg width={500} height={300}>
       <rect
         x={10 + 0 * (50 + 10)}
-        y={30}
-        height={300 - 250}
+        y={300 - 250}
+        height={250}
         width={50}
         fill="red"
       />
       <rect
         x={10 + 1 * (50 + 10)}
-        y={130}
-        height={300 - 200}
+        y={300 - 200}
+        height={200}
         width={50}
         fill="red"
       />
       <rect
         x={10 + 2 * (50 + 10)}
-        y={230}
-        height={300 - 150}
+        y={300 - 150}
+        height={150}
         width={50}
         fill="red"
       />
       <rect
         x={10 + 3 * (50 + 10)}
-        y={330}
-        height={300 - 100}
+        y={300 - 100}
+        height={100}
         width={50}
         fill="red"
       />
