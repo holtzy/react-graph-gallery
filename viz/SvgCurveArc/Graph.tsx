32f052a90c53e8b6870e7d47901a3f1ec5ea76a4
Holtz Yan <yan.holtz@datadoghq.com>
2024-10-22 18:45:25 +0200

    mm

--- viz/SvgCurveArc/Graph.tsx
+++ viz/SvgCurveArc/Graph.tsx
@@ -1,8 +1,10 @@
 export const Graph = () => {
   return (
     <svg width={500} height={300}>
-      <path d="C 20 30, 40 50, 60 70" stroke="blue" />
-      <path d="A 130 130 0 0 1 170 170" stroke="red" />
+      <svg width={500} height={300}>
+        <path d="M 20 30 C 40 50, 60 70, 80 30" stroke="blue" fill="none" />
+        <path d="M 130 130 A 30 30 0 0 1 170 170" stroke="red" fill="none" />
+      </svg>
     </svg>
   );
 };
