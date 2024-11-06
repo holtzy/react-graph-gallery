1fb18458c63d6c1e122ab2ead0c3833715b7e20c
Holtz Yan <yan.holtz@datadoghq.com>
2024-11-06 16:42:58 +0100

    first canvas cicle

--- viz/CanvasBasicCircle/Graph.tsx
+++ viz/CanvasBasicCircle/Graph.tsx
@@ -1,15 +1,33 @@
+import { useEffect, useRef } from 'react';
+
 type GraphProps = {
   width: number;
   height: number;
 };
 
 export const Graph = ({ width, height }: GraphProps) => {
-  const canvas = document.getElementById('myCanvas');
-  const ctx = canvas.getContext('2d');
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (!canvas) {
+      return;
+    }
+
+    const ctx = canvas.getContext('2d');
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, width, height);
+    ctx.beginPath(); // Start a new path (a new shape)
+    ctx.arc(100, 100, 50, 0, 2 * Math.PI); // Draw the circle
+    ctx.fillStyle = 'blue'; // Set the fill color to blue
+    ctx.fill(); // Fill the circle with color
+  }, []);
 
   return (
     <div>
-      <canvas id="myCanvas" width={width} height={height}></canvas>
+      <canvas ref={canvasRef} width={width} height={height} />
     </div>
   );
 };
