178624c59ba7e5ddfd92841d1795a7c778bab66c
Holtz Yan <yan.holtz@datadoghq.com>
2025-07-29 16:15:13 +0200

    add slider

--- viz/GenArtIntroPerlin/Plot.tsx
+++ viz/GenArtIntroPerlin/Plot.tsx
@@ -1,38 +1,52 @@
 'use client';
 
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState, useMemo } from 'react';
 import { createNoise2D } from 'simplex-noise';
 import { setupHiDPICanvas } from './setupHiDPICanvas';
 import { scaleLinear } from 'd3';
 
 type PlotProps = { width: number; height: number };
 
-const X_OFFSET = 0.04;
-
 export const Plot = ({ width, height }: PlotProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  const [xOffset, setXOffset] = useState(0.04);
+
   const yScale = scaleLinear().range([height, 0]).domain([-1, 1]);
+
+  const noise = useMemo(() => {
+    return createNoise2D();
+  }, []);
 
   useEffect(() => {
     const ctx = setupHiDPICanvas(canvasRef.current, width, height);
     if (!ctx) return;
     ctx.clearRect(0, 0, width, height);
 
-    const noise = createNoise2D();
-
     ctx.beginPath();
-
     for (let x = 0; x < width; x += 1) {
-      const value = noise(x * X_OFFSET, 0);
+      const value = noise(x * xOffset, 0);
       const y = yScale(value);
       ctx.lineTo(x, y);
     }
-
     ctx.strokeStyle = '#6a4c93';
     ctx.lineWidth = 2;
     ctx.stroke();
-  }, [width, height]);
+  }, [width, height, xOffset]);
 
-  return <canvas ref={canvasRef} style={{ display: 'block', width, height }} />;
+  return (
+    <div className="relative">
+      <input
+        className="absolute"
+        type="range"
+        min={0.001}
+        max={0.1}
+        value={xOffset}
+        step={0.001}
+        onChange={(e) => setXOffset(Number(e.target.value))}
+        style={{ height: 2, opacity: 0.5 }}
+      />
+      <canvas ref={canvasRef} style={{ display: 'block', width, height }} />;
+    </div>
+  );
 };
